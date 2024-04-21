565044f50c4cc128cd36b0148d7c39e1f83302ec
Muhammad Tanveer <muhammadtanveer0135@gmail.com>
2024-04-21 02:32:37 -0700

    comment added

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { userRegister } = require("../controls/user");
 const route = express.Router();
 
-
+// this is comment
 
 route.post("/register",userRegister)
 
